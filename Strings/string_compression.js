27b5a92958096433c91f5c0d6984c713aac8e529
Salvatore Santamaria <sam.santamaria@gmail.com>
2022-08-15 15:06:27 -0700

    Add slight version to string compression

--- Strings/string_compression.js
+++ Strings/string_compression.js
@@ -11,3 +11,22 @@ function compress(str) {
 	}
   console.log(result)
 }
+
+//compress(["a","a","b","b","c","c","c"])
+
+
+var compress2 = function(chars) {
+	let count = 1
+	let result = ''
+	for (let i = 0; i < chars.length; i++) {
+			if (chars[i] !== chars[i + 1]) {
+					count++
+			} else {
+					result = result + chars[i] + count
+					count = 1
+			}
+	}
+	console.log(result)
+}
+
+compress2(["a","a","b","b","c","c","c"])
